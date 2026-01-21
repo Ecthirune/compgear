869f75151055c9fe15ffd2780d16758e49c78bb1
Juneluya <juneluya@21-school.ru>
2026-01-21 16:45:31 +0300

    FIX: small cleanup from redunant modules

--- data/affix_sorter.h
+++
@@ -1,3 +0,0 @@
-#include <string>
-
-bool create_data_files(std::string input_path);
--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <d3d11.h>
 #include <dwmapi.h>
 #include <windows.h>
-#include "data/affix_sorter.h"
 #include <fstream>
 #pragma comment(lib, "dwmapi.lib")
 #pragma comment(lib, "d3d11.lib")
@@ -16,11 +15,5 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
   auto fn = reinterpret_cast<SetProcessDpiAwarenessContextFunc>(GetProcAddress(
       GetModuleHandleW(L"user32.dll"), "SetProcessDpiAwarenessContext"));
   if (fn) fn((HANDLE)-4);  // DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2
-      // if (create_data_files("input.json")){
-      // }
-      // else{
-      //   MessageBoxA(NULL, "Can't find input.json, terminating", "Error", MB_OK | MB_ICONERROR);
-      //   return 1;
-      // };
   return imgui_init() ? 0 : 1;
 }
